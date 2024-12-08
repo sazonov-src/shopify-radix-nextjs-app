8f7f542eeddce0bc1e691807766395fd2b9c3f13
Vitalii Sazonov <vitaliy89vin@gmail.com>
2024-12-08 17:05:11 +0200

    create cart dialog base

---
+++ app/cart/page.tsx
@@ -0,0 +1,7 @@
+import Cart from "@/components/shopify/Cart";
+
+export default function CartPage() {
+  return (
+    <Cart />
+  );
+}
--- components/shopify/Cart.tsx
+++ components/shopify/Cart.tsx
@@ -1,15 +1,92 @@
 "use client";
+import {
+  Box,
+  Dialog,
+  Flex,
+  Link,
+  Text,
+  Select,
+  Separator,
+  Strong,
+  Button,
+} from "@radix-ui/themes";
 import { useCart } from "@shopify/hydrogen-react";
 
 export default function Cart() {
   const { status, lines, note } = useCart();
   return (
-    <div>
-      <h1>Cart {status}</h1>
-      <p>Note: {note}</p>
-      {lines?.map((line) => (
-       <p>{line?.merchandise?.title}</p>
-      ))}
-    </div>
+    <Dialog.Root>
+      <Dialog.Trigger>
+        <Button>Edit profile</Button>
+      </Dialog.Trigger>
+
+      <Dialog.Content maxWidth="550px">
+        <Dialog.Title>Shopping cart</Dialog.Title>
+        
+          <Flex direction="column" gap="4" mt="4">
+            {lines?.map((item) => (
+              <Flex gap="4" align="center" justify="between" key={item?.id}>
+                <Flex flexGrow="1" align="center" gap="2" >
+                  <img
+                    src={item?.merchandise?.image?.url}
+                    style={{ borderRadius: "var(--radius-1)" }}
+                    width="64"
+                    height="64"
+                  />
+                  <Box>
+                    <Text as="div" color="gray" trim="start">
+                      <Link
+                        href="#"
+                        underline="hover"
+                        size="2"
+                        weight="bold"
+                        onClick={(e) => e.preventDefault()}
+                      >
+                        {item?.merchandise?.product?.title}
+                      </Link>
+                    </Text>
+                    <Text as="div" color="gray" size="1" trim="end">
+                      {item?.merchandise?.title}
+                    </Text>
+                  </Box>
+                </Flex>
+
+                <Flex direction="column" >
+                  <Select.Root defaultValue={item?.quantity?.toString()} size="1">
+                    <Select.Trigger />
+                    <Select.Content variant="soft" position="popper">
+                      {Array.from({ length: 9 }, (_, i) => (
+                        <Select.Item key={i} value={String(i + 1)}>
+                          {i + 1}
+                        </Select.Item>
+                      ))}
+                    </Select.Content>
+                  </Select.Root>
+                </Flex>
+
+                <Flex direction="column" >
+                  <Text size="2" weight="bold" align="right">
+                    {item?.cost?.totalAmount?.amount}
+                  </Text>
+                </Flex>
+              </Flex>
+            ))}
+          </Flex>
+
+          <Box>
+            <Separator size="4" my="4" />
+          </Box>
+
+          <Flex align="center" justify="between" mt="4">
+            <Text size="2">
+              Total <Strong>$313</Strong>
+            </Text>
+
+            <Button size="2" variant="solid" >
+              Go to checkout
+            </Button>
+          </Flex>
+      </Dialog.Content>
+    </Dialog.Root>
   );
 }
