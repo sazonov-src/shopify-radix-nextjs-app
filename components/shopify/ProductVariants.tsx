cf62efd06792f9a0c9916ee6a190778c64545cbe
Vitalii Sazonov <vitaliy89vin@gmail.com>
2024-12-12 17:05:27 +0200

    changed buttons to radio-cards in produnt variants

--- components/shopify/ProductVariants.tsx
+++ components/shopify/ProductVariants.tsx
@@ -1,33 +1,46 @@
 "use client";
 import React from "react";
 import { useProduct } from "@shopify/hydrogen-react";
-import { Flex, Button, Box } from "@radix-ui/themes";
+import { Flex, RadioCards, Text, Box } from "@radix-ui/themes";
+import { useEffect } from "react";
 
 export default function ProductVariants() {
-  const { variants, setSelectedVariant, selectedVariant } = useProduct();
+  const { options, variants, setSelectedVariant, selectedVariant } = useProduct();
+
+  useEffect(() => {
+    console.log(options)
+  }, []);
 
   return (
     variants && (
       <Box>
-        <Flex gap="2">
+        <RadioCards.Root
+          defaultValue={selectedVariant?.id}
+          columns={{ initial: "1", sm: "3" }}
+        >
           {variants.map(
             (variant) =>
-              variant && variant.title !== "Default Title" && (
-                <Button
-                  variant={
-                    variant?.id === selectedVariant?.id ? "solid" : "outline"
-                  }
-                  onClick={() => { 
-                    setSelectedVariant(variant) 
-                    console.log("selected variant", variant)
-                  }}
+              variant &&
+              variant.id &&
+              variant.title !== "Default Title" && (
+                <RadioCards.Item
+                  onClick={() => setSelectedVariant(variant)}
                   key={variant.id}
+                  value={variant.id}
                 >
-                  {variant.title}
-                </Button>
+                  <Flex direction="column" width="100%">
+                    { variant?.selectedOptions && 
+                      variant.selectedOptions.map((option) => (
+                      <Text size="1" key={option?.name}>
+                        {option?.name}
+                      </Text>
+                    ))}
+                    <Text weight="bold">{variant.title}</Text>
+                  </Flex>
+                </RadioCards.Item>
               )
           )}
-        </Flex>
+        </RadioCards.Root>
       </Box>
     )
   );
